e885d19cd319a65a2034e1637d1d796232144925
Roman Zimmermann <roman@more-onion.com>
2019-08-22 13:13:09 +0200

    Server-side handling for additional values.

--- src/CreditCardForm.php
+++ src/CreditCardForm.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\braintree_payment;
 
-use \Braintree\ClientToken;
-use \Braintree\Configuration;
-use \Drupal\payment_forms\CreditCardForm as _CreditCardForm;
+use Braintree\ClientToken;
+use Braintree\Configuration;
+use Drupal\little_helpers\ElementTree;
+use Drupal\payment_forms\CreditCardForm as _CreditCardForm;
 
 /**
  * @file
@@ -49,32 +50,84 @@ class CreditCardForm extends _CreditCardForm {
       '#attributes' => ['data-braintree-name' => 'amount'],
     ];
 
-    $data = $method->controller_data['billing_data'];
-    $default = ['keys' => [], 'required' => FALSE, 'display' => 'hidden'];
-    $context = $payment->contextObj;
-    $bd = static::extraDataFields();
-    foreach ($bd as $name => &$field) {
-      $config = isset($data[$name]) ? $data[$name] + $default : $default;
-      $field['#controller_required'] = $config['required'];
-      $field['#default_value'] = '';
-      if ($context) {
-        foreach ($config['keys'] as $key) {
-          if ($value = $context->value($key)) {
-            $field['#default_value'] = $value;
-            break;
+    $data_fieldset = static::extraElements();
+    $data_fieldset['#settings'] = $method->controller_data['input_settings'];
+
+    // Recursively set #settings and remove #required.
+    ElementTree::applyRecursively($data_fieldset, function(&$element, $key, &$parent) {
+      if ($key) {
+        $element['#settings'] = $parent['#settings'][$key];
+      }
+      $element['#controller_required'] = !empty($element['#required']);
+      unset($element['#required']);
+      if (!empty($element['#braintree_field'])) {
+        $element['#attributes']['data-braintree-name'] = $element['#braintree_field'];
+      }
+      $element['#user_visible'] = FALSE;
+
+    });
+
+    // Set default values from context.
+    if ($context = $payment->contextObj) {
+      ElementTree::applyRecursively($data_fieldset, function (&$element, $key) use ($context) {
+        if (!in_array($element['#type'], ['container', 'fieldset'])) {
+          foreach ($element['#settings']['keys'] as $k) {
+            if ($value = $context->value($k)) {
+              $element['#default_value'] = $value;
+              break;
+            }
           }
         }
-      }
-      if (!$this->shouldDisplay($field, $config['display'])) {
-        $field['#type'] = 'hidden';
-        $field['#value'] = $field['#default_value'];
-        $field['#attributes']['data-braintree-name'] = 'billing_data.' . $field['#braintree_field'];
-      }
+      });
     }
 
-    $form['billing_data'] = $bd + [
-      '#type' => 'container',
-    ];
+    $display = function ($element, $key, $mode = 'display') {
+      $d = $element['#settings'][$mode];
+      return ($d == 'always') || (empty($element['#default_value']) && $d == 'ifnotset');
+    };
+
+    // Set visibility.
+    ElementTree::applyRecursively($data_fieldset, function (&$element, $key, &$parent) use ($display) {
+      $element += ['#access' => FALSE];
+      $is_container = in_array($element['#type'], ['fieldset', 'container']);
+      if (!$is_container) {
+        $element['#access'] = $element['#settings']['enabled'];
+      }
+      // If an element is accessible its parent should be visible too.
+      if ($parent && $element['#access']) {
+        $parent['#access'] = TRUE;
+      }
+
+      if (!$is_container) {
+        $element['#user_visible'] = $display($element, $key, 'display');
+      }
+      if ($element['#user_visible'] && $parent) {
+        $parent['#user_visible'] = TRUE;
+      }
+    }, TRUE);
+    // Reset visibility if there are visible elements in the same fieldset.
+    ElementTree::applyRecursively($data_fieldset, function (&$element, $key, &$parent) use ($display) {
+      if ($parent && $parent['#user_visible']) {
+        // Give child elements of visible fieldsets a chance to be displayed.
+        if ($element['#type'] != 'fieldset' && !$element['#user_visible']) {
+          $element['#user_visible'] = $display($element, $key, 'display_other');
+        }
+      }
+    });
+    // Transform elements that should not be visible for the user.
+    ElementTree::applyRecursively($data_fieldset, function (&$element, $key, &$parent) use ($display) {
+      if ($key && !$element['#user_visible']) {
+        if ($element['#type'] == 'fieldset') {
+          $element['#type'] = 'container';
+        }
+        else {
+          $element['#type'] = 'hidden';
+          $element['#value'] = $element['#default_value'];
+        }
+      }
+    });
+
+    $form['extra_data'] = ['#weight' => 100] + $data_fieldset;
     return $form;
   }
 
@@ -145,61 +198,119 @@ class CreditCardForm extends _CreditCardForm {
    *   A form-API style array defining fields that map to the braintree billing
    *   data using the #braintree_field attribute.
    */
-  public static function extraDataFields() {
+  public static function extraElements() {
     require_once DRUPAL_ROOT . '/includes/locale.inc';
-    $fields['first_name'] = [
+
+    $fields = [
+      '#type' => 'container',
+    ];
+
+    $fields['email'] = [
+      '#type' => 'textfield',
+      '#title' => t('Email address'),
+      '#braintree_field' => 'email',
+    ];
+
+    $fields['billing_address'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Billing address'),
+    ];
+    $fields['billing_address']['first_name'] = [
       '#type' => 'textfield',
       '#title' => t('First name'),
-      '#braintree_field' => 'firstName',
+      '#braintree_field' => 'billingAddress.firstName',
     ];
-
-    $fields['last_name'] = [
+    $fields['billing_address']['last_name'] = [
       '#type' => 'textfield',
       '#title' => t('Last name'),
-      '#braintree_field' => 'lastName',
+      '#braintree_field' => 'billingAddress.lastName',
     ];
-
-    $fields['company'] = [
+    $fields['billing_address']['company'] = [
       '#type' => 'textfield',
       '#title' => t('Company'),
-      '#braintree_field' => 'company',
+      '#braintree_field' => 'billingAddress.company',
     ];
-
-    $fields['street_address'] = [
+    $fields['billing_address']['street_address'] = [
       '#type' => 'textfield',
       '#title' => t('Address line 1'),
-      '#braintree_field' => 'streetAddress',
+      '#braintree_field' => 'billingAddress.streetAddress',
     ];
-
-    $fields['address_line2'] = [
+    $fields['billing_address']['address_line2'] = [
       '#type' => 'textfield',
       '#title' => t('Address line 2'),
-      '#braintree_field' => 'extendedAddress',
+      '#braintree_field' => 'billingAddress.extendedAddress',
     ];
-
-    $fields['country'] = [
+    $fields['billing_address']['country'] = [
       '#type' => 'select',
       '#options' => country_get_list(),
       '#title' => t('Country'),
-      '#braintree_field' => 'countryCodeAlpha2',
+      '#braintree_field' => 'billingAddress.countryCodeAlpha2',
     ];
-
-    $fields['postcode'] = [
+    $fields['billing_address']['postcode'] = [
       '#type' => 'textfield',
       '#title' => t('Postal code'),
-      '#braintree_field' => 'postalCode',
+      '#braintree_field' => 'billingAddress.postalCode',
     ];
-
-    $fields['city'] = [
+    $fields['billing_address']['city'] = [
       '#type' => 'textfield',
       '#title' => t('City/Locality'),
-      '#braintree_field' => 'locality',
+      '#braintree_field' => 'billingAddress.locality',
     ];
-
-    $fields['region'] = [
+    $fields['billing_address']['region'] = [
       '#type' => 'textfield',
       '#title' => t('Region/State'),
-      '#braintree_field' => 'region',
+      '#braintree_field' => 'billingAddress.region',
+    ];
+
+    $fields['shipping_address'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Shipping address'),
+    ];
+    $fields['shipping_address']['given_name'] = [
+      '#type' => 'textfield',
+      '#title' => t('Given name'),
+      '#braintree_field' => 'additionalInformation.shippingGivenName',
+    ];
+    $fields['shipping_address']['surname'] = [
+      '#type' => 'textfield',
+      '#title' => t('Surname'),
+      '#braintree_field' => 'additionalInformation.shippingSurname',
+    ];
+    $fields['shipping_address']['street_address'] = [
+      '#type' => 'textfield',
+      '#title' => t('Street address'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.streetAddress',
+    ];
+    $fields['shipping_address']['extended_address'] = [
+      '#type' => 'textfield',
+      '#title' => t('Extended address'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.extendedAddress',
+    ];
+    $fields['shipping_address']['line3'] = [
+      '#type' => 'textfield',
+      '#title' => t('Address line 3'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.line3',
+    ];
+    $fields['shipping_address']['locality'] = [
+      '#type' => 'textfield',
+      '#title' => t('Locality (city)'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.locality',
+    ];
+    $fields['shipping_address']['region'] = [
+      '#type' => 'textfield',
+      '#title' => t('Region'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.region',
+    ];
+    $fields['shipping_address']['postal_code'] = [
+      '#type' => 'textfield',
+      '#title' => t('Postal code'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.postalCode',
+    ];
+    $fields['shipping_address']['country'] = [
+      '#type' => 'select',
+      '#title' => t('Country'),
+      '#braintree_field' => 'additionalInformation.shippingAddress.countryCodeAlpha2',
+      '#options' => country_get_list(),
     ];
 
     return $fields;
