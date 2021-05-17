4b0b227d2329fbb61cbff3acf512ff4b713c60d0
Katharina Zwinger <katharina@more-onion.com>
2021-05-17 12:46:25 +0200

    JS: spilt up error handler

--- package/src/method-element.js
+++ package/src/method-element.js
@@ -22,6 +22,7 @@ class MethodElement {
       wrapperClasses: ['form-control'],
     })
     this.form_id = this.$element.closest('form').attr('id')
+    this.errorHandler = this.clientsideValidationEnabled ? this.clientsideValidationErrorHandler : this.fallbackErrorHandler
     this.waitForLibrariesThenInit()
   }
 
@@ -86,45 +87,50 @@ class MethodElement {
   }
 
   /**
-   * Display error messages.
+   * Displays error messages using Drupal Clientside Validation.
+   *
    * @param {object} error - The Braintree error data.
    * @param {jquery} $field - The field that caused the error.
    */
-  errorHandler (error, $field = null) {
-    // Trigger clientside validation for respective field.
-    if (this.clientsideValidationEnabled()) {
-      const validator = Drupal.myClientsideValidation.validators[this.form_id]
-      if ($field && $field.attr('name')) {
-        const errors = {}
-        errors[$field.attr('name')] = error
-        // Needed so jQuery validate will find the element when removing errors.
-        validator.currentElements.push($field)
-        // Trigger validation error.
-        validator.showErrors(errors)
-      }
-      else {
-        // The error is not related to a payment field, reconstruct error markup.
-        const settings = Drupal.settings.clientsideValidation.forms[this.form_id].general
-        const $message = $(`<${settings.errorElement} class="${settings.errorClass}">`).text(error)
-        const $wrapper = $('#clientsidevalidation-' + this.form_id + '-errors')
-        // Add message to clientside validation wrapper if there is one.
-        if ($wrapper.length) {
-          const $list = $wrapper.find('ul')
-          $message.wrap(`<${settings.wrapper}>`).parent().addClass('braintree-error').appendTo($list)
-          $list.show()
-          $wrapper.show()
-        }
-        // Show message above the payment fieldset in want of a better place.
-        else {
-          $message.addClass('braintree-error').insertBefore(this.$element)
-        }
-      }
+  clientsideValidationErrorHandler (error, $field = null) {
+    const validator = Drupal.myClientsideValidation.validators[this.form_id]
+    if ($field && $field.attr('name')) {
+      // Trigger clientside validation for respective field.
+      const errors = {}
+      errors[$field.attr('name')] = error
+      // Needed so jQuery validate will find the element when removing errors.
+      validator.currentElements.push($field)
+      // Trigger validation error.
+      validator.showErrors(errors)
     }
-    // Without clientside validation render a message above the form.
     else {
-      const $message = $('<div class="messages error">').text(error)
-      $message.addClass('braintree-error').insertBefore(this.$element.closest('form'))
+      // The error is not related to a payment field, reconstruct error markup.
+      const settings = Drupal.settings.clientsideValidation.forms[this.form_id].general
+      const $message = $(`<${settings.errorElement} class="${settings.errorClass}">`).text(error)
+      const $wrapper = $('#clientsidevalidation-' + this.form_id + '-errors')
+      // Add message to clientside validation wrapper if there is one.
+      if ($wrapper.length) {
+        const $list = $wrapper.find('ul')
+        $message.wrap(`<${settings.wrapper}>`).parent().addClass('braintree-error').appendTo($list)
+        $list.show()
+        $wrapper.show()
+      }
+      // Show message above the payment fieldset in want of a better place.
+      else {
+        $message.addClass('braintree-error').insertBefore(this.$element)
+      }
     }
+  }
+
+  /**
+   * Displays error messages without Drupal Clientside Validation.
+   *
+   * @param {object} error - The Braintree error data.
+   */
+  fallbackErrorHandler (error) {
+    // Render a message above the form.
+    const $message = $('<div class="messages error">').text(error)
+    $message.addClass('braintree-error').insertBefore(this.$element.closest('form'))
   }
 
   /**
